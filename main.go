6e733ce6672030291c627db8989ae639475abee1
pbouquer <p.bouqueret@gmail.com>
2026-02-09 14:25:03 +0100

    feat(server): setup http server and static files

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
+	"real-time-forum/database"
+	"real-time-forum/websocket"
 )
 
 func main() {
-    port := "8080"
-    if envPort := os.Getenv("PORT"); envPort != "" {
-        port = envPort
-    }
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
 
-    fs := http.FileServer(http.Dir("./static"))
-    http.Handle("/", fs)
+	// Initialize Database
+	dbPath := "./forum.db"
+	err := database.InitDB(dbPath)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer database.DB.Close()
 
-    log.Printf("Server starting on http://localhost:%s", port)
-    err := http.ListenAndServe(":"+port, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Initialize WebSocket Hub
+	hub := websocket.NewHub()
+	go hub.Run()
+
+	// Serve Static Files
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	// WebSocket Endpoint
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		websocket.ServeWs(hub, w, r)
+	})
+
+	log.Printf("Server starting on http://localhost:%s", port)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
